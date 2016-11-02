19e697a1d5fdec811e8704bf7d1866ff12e8eae2
Pedro Paredes <ppa2290@gmail.com>
2016-11-02 23:40:32 +0100

    Added linting configuration with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "object-curly-spacing": ["error", "always"],
+        "comma-dangle": ["error", "always-multiline"],
+        "react/jsx-uses-react": 1,
+        "react/jsx-uses-vars": 1,
+    }
+};
