c7bcbe21616bf220d583e17321295c9e04339af3
Pedro Paredes <ppa2290@gmail.com>
2016-11-16 19:50:47 +0100

    Added redux logger when in dev environment

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,18 @@
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import root from '../reducers/'
 
+const middlewares = []
 
-export default createStore.bind(null, root)
+// Due to problems with Windows environments, environment is default to 'development'
+const environment = process.env.NODE_ENV || 'development'
+if (environment === 'development') {
+  const createLogger = require('redux-logger')
+  const logger = createLogger()
+  middlewares.push(logger)
+}
+
+export default createStore.bind(
+  null,
+  root,
+  applyMiddleware(...middlewares)
+)
